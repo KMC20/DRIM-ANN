5bbe6e01a5659db656fa3920f2449165a1a725ae
KMC20 <74809860+KMC20@users.noreply.github.com>
2025-08-26 20:48:50 +0800

    Update model.sh

--- Perf-Model/model.sh
+++ Perf-Model/model.sh
@@ -1,1 +1,7 @@
+if [ ! -d Excels ]; then
+    mkdir Excels
+fi
+if [ ! -d Figures ]; then
+    mkdir Figures
+fi
 python perf_model.py --log-file-name ../DRIM-ANN/build/output.txt --dpu-amount 1018 --host-thread-amount 20
